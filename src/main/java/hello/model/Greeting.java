bc371198157283a6a89f6a5c0a71ca07b322623e
Jakub Korocinski <jakub.korocinski@mga.com.pl>
2018-05-31 07:28:09 +0200

    Created REST Greeting Controller.

---
+++ src/main/java/hello/model/Greeting.java
@@ -0,0 +1,68 @@
+package hello.model;
+
+public class Greeting {
+	
+	long id;
+	String content;
+	
+	public Greeting(long id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Greeting other = (Greeting) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Greeting [id=" + id + ", content=" + content + "]";
+	}
+	
+	
+	
+	
+	
+
+}
